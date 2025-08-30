dcfdb9abfdff2d34030efd5f109923e35f5cfb6b
Sumit Kumar <Sumitlund325@gmail.com>
2025-08-30 00:14:32 -0700

    CONDITIONS.py

--- CONDITIONS.py
+++ CONDITIONS.py
@@ -1,52 +1,53 @@
-# a,b=20,40
-# if b>a:
-#     print(f"{b} is greater than {a}")
+a,b=20,40
+if b>a:
+    print(f"{b} is greater than {a}")
 
-# a,b=33,33
-# if a>b:
-#     print("a is greater than b")
-# elif a==b:
-#     print("a is equal to b")
+a,b=33,33
+if a>b:
+    print("a is greater than b")
+elif a==b:
+    print("a is equal to b")
 
-# a,b=33,300
-# if a>b:
-#     print("a is greater than b")
-# elif a==b:
-#     print("a is equal to b")
-# else:
-#     print("b is greater than a")
+a,b=33,300
+if a>b:
+    print("a is greater than b")
+elif a==b:
+    print("a is equal to b")
+else:
+    print("b is greater than a")
 
-# one line statement
-# a=2
-# b=1
-# if a>b: print("a is greater than b")
+one line statement
+a=2
+b=1
+if a>b: print("a is greater than b")
 
-# a,b=1,2
-# print("A") if a>b else print("B")
+a,b=1,2
+print("A") if a>b else print("B")
 
-# a=33
-# b=33
-# print("A") if a>b else print("=") if a==b else print("B")
+a=33
+b=33
+print("A") if a>b else print("=") if a==b else print("B")
 
-# a=1
-# b=2
-# c=3
-# if a<b and b<c:
-#     print("both conditions are true")
-# if a<b or b>c:
-#     print("at least one condition is true")
-# if not a>b:
-#     print("a is not greater than b")
+a=1
+b=2
+c=3
+if a<b and b<c:
+    print("both conditions are true")
+if a<b or b>c:
+    print("at least one condition is true")
+if not a>b:
+    print("a is not greater than b")
 
-# x=11
-# if x>10:
-#     print("x is greater than 10,")
-#     if x>20:
-#         print("x is also greater than 20")
-#     else:
-#         print("but not above 20")
+x=11
+if x>10:
+    print("x is greater than 10,")
+    if x>20:
+        print("x is also greater than 20")
+    else:
+        print("but not above 20")
 
-# a=33
-# b=10
-# if a>b:
-#     pass
+a=33
+b=10
+if a>b:
+    pass
+
